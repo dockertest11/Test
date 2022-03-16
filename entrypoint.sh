46d1b2658d456bcf2feb22c9a868a7df5a01a94a
dockertest11 <101738077+dockertest11@users.noreply.github.com>
2022-03-16 21:38:37 +0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -z "${PASSWORD}" ]]; then
-  export PASSWORD="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+  export PASSWORD="7dg96nd9-df89-98sd-9asp-hnd9obs60md9ap"
 fi
 echo ${PASSWORD}
 
